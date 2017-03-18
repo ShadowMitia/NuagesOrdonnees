7d894e519be4d08fbd99bf821217e16c484b220e
Dimitri Belopopsky <dimitri@belopopsky.com>
2017-03-18 17:59:52 +0100

    Ajout des boids

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,7 +1,7 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
   videoStream.setup(640, 480);
 
   background.setIgnoreForeground(true);
@@ -10,10 +10,15 @@ void ofApp::setup(){
   gui.add(resetBackground.set("Reset Background", false));
   gui.add(learningTime.set("Learning Time", 30, 0, 30));
   gui.add(thresholdValue.set("Threshold Value", 10, 0, 255));
+
+  boids.BoidsSetup();
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::update() {
+  if (boids.isMainThread()) {
+    boids.startThread();
+  }
   videoStream.update();
   if(resetBackground) {
     background.reset();
@@ -30,11 +35,22 @@ void ofApp::update(){
     contourFinder.setThreshold(thresholdValue);
     contourFinder.findContours(videoStream);
     ofxCv::toOf(background.getBackground(), backgroundImage);
+    backgroundImage.update();
+    ofxCv::subtract(backgroundImage, videoStream, contourImage);
+    contourImage.update();
+    contourImage.setImageType(OF_IMAGE_GRAYSCALE);
+    contourFinder2.setMinAreaRadius(60);
+    contourFinder2.setMaxAreaRadius(1000);
+    contourFinder2.setThreshold(40);
+    contourFinder2.findContours(contourImage);
+
   }
+
+
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
   videoStream.draw(0, 0);
   if(thresholded.isAllocated()) {
     thresholded.draw(640, 0);
@@ -47,6 +63,19 @@ void ofApp::draw(){
 
 
   backgroundImage.draw(0, 480);
+  contourImage.draw(640, 480);
+  ofPushMatrix();
+  ofTranslate(640, 480);
+  contourFinder2.draw();
+  ofPopMatrix();
+
+  ofSetColor(0, 0, 0, 25);
+  for (int i = 0; i< boids.getBoids()->size(); i++) {
+    Boid2d  *b = boids.flock.totalBoid.at(i);
+    ofDrawRectangle(b->position.x, b->position.y, 5,5);
+    float lm = 10.f;
+    ofDrawLine(b->position.x, b->position.y, b->position.x + b->velocite.x*lm, b->position.y + b->velocite.y*lm);
+  }
 
   gui.draw();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,41 +1,90 @@
+//-*- mode: c++ -*-
 #pragma once
-
 
 
 #include "ofMain.h"
 #include "ofxOpenCv.h"
 #include "ofxCv.h"
 #include "ofxGui.h"
+#include "Flock2d.h"
+
+class BoidsThread: public ofThread {
+public:
+    Flock2d flock;
+    BoidsThread(){
+        flock.setBounds(0, 0, ofGetWindowWidth()  , ofGetWindowHeight());
+        flock.setBoundmode(1);
+    };
+
+    void BoidsSetup(){
+        int unit= 30;
+        flock.isVectorField=false;
+        for (int i = (ofGetWidth()/unit); i < ofGetWidth(); i = i + ofGetWidth()/unit) {
+            for(int j = (ofGetHeight()/unit);  j < ofGetHeight(); j = j+ ofGetHeight()/unit){
+                flock.addBoid(ofVec2f(i, j),7, 10, 5, 50, 6, 60, 2, 2);
+            }
+        }
+        cout << flock.totalBoid.size();
+    };
+
+    vector<Boid2d*>* getBoids(){
+        return &flock.totalBoid;
+    };
+
+    void setBoidsActif(vector<Boid2d*> _boidsActif){
+        boidsActif = _boidsActif;
+    };
+
+private:
+    vector<Boid2d *> boidsActif;
+    vector<vector<ofVec2f>> *vect;
+
+
+    void threadedFunction(){
+        //boidsActif=flock.totalBoid;
+        //flock.update(&boidsActif, &boidsActif, vect);
+        flock.update(&flock.totalBoid, &flock.totalBoid, vect);
+    };
+};
+
 
 
 class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+  void setup();
+  void update();
+  void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y );
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void mouseEntered(int x, int y);
+  void mouseExited(int x, int y);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
 
-		// Stuff for background detection
-		ofVideoGrabber videoStream;
-		ofxCv::RunningBackground background;
-		ofImage thresholded;
-		ofxPanel gui;
-		ofParameter<bool> resetBackground;
-		ofParameter<float> learningTime, thresholdValue;
+  BoidsThread boids;
 
-		ofxCv::ContourFinder contourFinder;
+  // Camera stream
+  ofVideoGrabber videoStream;
+  // Background detection things
+  ofxCv::RunningBackground background;
+  ofImage thresholded;
+  ofImage backgroundImage;
+  // GUI stuff
+  ofxPanel gui;
+  ofParameter<bool> resetBackground;
+  ofParameter<float> learningTime, thresholdValue;
 
-		ofImage backgroundImage;
+  // Contour Finder
+  ofxCv::ContourFinder contourFinder;
+  // Contour Finder
+  ofxCv::ContourFinder contourFinder2;
+
+  ofImage contourImage;
 };
